845d4cf1f79e727e00fada6c01d5bbaeeab99ddf
Stephen Kelly <stevekm@users.noreply.github.com>
2017-05-12 17:29:16 -0400

    add variant quality barplot and spinner run function

--- run.sh
+++ run.sh
@@ -4,6 +4,21 @@
 ## EXAMPLE: run.sh "NS17-03" "RUN_4"
 ## DESCRIPTION: This script will set up and run the downstream analysis for the WES pipeline
 
+spinner()
+{
+    local pid=$!
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
 # ~~~~~ CHECK SCRIPT ARGS ~~~~~ #
 if (( "$#" != "2" )); then
     echo "ERROR: Wrong number of arguments supplied"
@@ -28,11 +43,16 @@ printf "%s" "$results_ID" > results_ID.txt
 # get the annotations for all samples
 gatk_annotations_file="${project_ID}_${results_ID}_GATK_annotations.tsv"
 printf "Find all GATK variants...\n"
-find "run_analysis_output/VCF-GATK-HC-annot/" -name "*.combined.txt" | xargs ./toolbox/concat_tables.py > "$gatk_annotations_file" && printf "Variants saved to file:\n%s\n\n" "$gatk_annotations_file"
+
+(find "run_analysis_output/VCF-GATK-HC-annot/" -name "*.combined.txt" | xargs ./toolbox/concat_tables.py > "$gatk_annotations_file" && printf "Variants saved to file:\n%s\n\n" "$gatk_annotations_file") &
+spinner
+
+
 
 lofreq_annotations_file="${project_ID}_${results_ID}_LoFreq_annotations.tsv"
 printf "Finding all LoFreq variants...\n"
-find "run_analysis_output/VCF-LoFreq-annot/" -name "*.combined.txt" | xargs ./toolbox/concat_tables.py > "$lofreq_annotations_file" && printf "Variants saved to file:\n%s\n\n" "$lofreq_annotations_file"
+(find "run_analysis_output/VCF-LoFreq-annot/" -name "*.combined.txt" | xargs ./toolbox/concat_tables.py > "$lofreq_annotations_file" && printf "Variants saved to file:\n%s\n\n" "$lofreq_annotations_file") & 
+spinner
 
 # make a copy of the report template file
 report_file="${project_ID}_${results_ID}_analysis_report.Rmd"
